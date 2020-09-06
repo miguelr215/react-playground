8bee4307c909dad025d554bc434945000dd8357a
drexelramos <drexelramos@gmail.com>
2020-09-05 20:56:05 -0700

    added tooltips

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Split from './composition/Split';
 import './App.css';
+import Tooltip from './composition/Tooltip';
+
+// make 2 tooltips here and 1 inside the App directly
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tootip message'>
+    ipsum
+  </Tooltip>
+);
+
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
 
 function App() {
   return (
     <main className='App'>
       <Split className="left" flexBasis={2}>
-      This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?  
+      This is the content for the left `Split`. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia? 
+      <Tooltip message='one more tooltip message'>
+        Necessitatibus?
+      </Tooltip> 
       </Split>
       <Split className="right">
-      This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
       </Split>
     </main>
   );
