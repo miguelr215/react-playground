f3eab05f27bf71c2330ba2838157c23408980099
drexelramos <drexelramos@gmail.com>
2020-09-29 06:21:51 -0700

    tabs functional and added enzyme

--- src/state/Tabs.js
+++ src/state/Tabs.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 class Tabs extends React.Component {
     static defaultProps = { tabs: [] };
+    state = {
+        currentTabIndex: 0
+    };
+    handleButtonClick(index){
+        this.setState({currentTabIndex: index})
+    }
     renderButtons(){
         return(
             this.props.tabs.map((tab, index) => (
-            <button key={index}>
+            <button key={index} onClick={() => this.handleButtonClick(index)}>
                 {tab.name}
             </button>
         ))
         )
     }
     renderContent(){
-        const currentTab = this.props.tabs[0];
+        const currentTab = this.props.tabs[this.state.currentTabIndex];
         return(
             <div className='content'>{currentTab.content}</div>
         )
