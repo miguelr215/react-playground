ead0e5df795bbf1c23469e536a133914660dfbd2
drexelramos <drexelramos@gmail.com>
2020-11-19 05:44:39 -0800

    added AppLang component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 // import App from './App';
-import App from './paltrows-power-toes/App';
+// import App from './paltrows-power-toes/App';
+import AppLang from './lang-context/AppLang';
 import './index.css';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <AppLang />
     </BrowserRouter>, 
     document.getElementById('root'));
---
+++ src/lang-context/AppLang.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './AppLang.css';
+
+export default class AppLang extends Component {
+    render() {
+      return (
+        <div className='AppLang'>
+          App context
+        </div>
+      );
+    }
+  }
+  
