04dcf24c667939a632e72a857087dbad6942c2cb
drexelramos <drexelramos@gmail.com>
2020-11-21 05:41:31 -0800

    added 2 ways to pass context

--- src/lang-context/LangControls.js
+++ src/lang-context/LangControls.js
@@ -4,27 +4,30 @@ import LanguageContext from './LanguageContext'
 export default function LangControls(props) {
   return (
       <LanguageContext.Consumer>
-          {/* { function renderProp() { */}
+          {/* value of LanguageContext */}
           {(value) => {
-              console.log(value)
               return(
                 <>
-                <button>
+                <button
+                    disabled={value.lang === 'en-GB'}>
                     British{' '}
                     <span role='img' aria-label='en-GB'>🇬🇧</span>
                 </button>
                 {' '}
-                <button>
+                <button
+                    disabled={value.lang === 'en-US'}>
                     American{' '}
                     <span role='img' aria-label='en-US'>🇺🇸</span>
                 </button>
                 {' '}
-                <button>
+                <button
+                    disabled={value.lang === 'ko'}>
                     Korean{' '}
                     <span role='img' aria-label='ko'>🇰🇷</span>
                 </button>
                 {' '}
-                <button>
+                <button
+                    disabled={value.lang === 'spanish'}>
                     Spanish{' '}
                     <span role='img' aria-label='spanish'>ESP</span>
                 </button>
