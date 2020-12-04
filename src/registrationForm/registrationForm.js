fcf8af9af9a70b5ed4aa30e8388cad86e6b591d8
drexelramos <drexelramos@gmail.com>
2020-12-04 06:03:12 -0800

    adding registration form

---
+++ src/registrationForm/registrationForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './registrationForm.css';
+
+export default class registrationForm extends Component {
+    render(){
+        return(
+            <form className='registration'>
+                
+            </form>
+        )
+    }
+}
