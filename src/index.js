512f723cdd6071cb019d22f80a5545115467a2a5
drexelramos <drexelramos@gmail.com>
2020-12-06 05:16:53 -0800

    updating registration form

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 // import App from './App';
 // import App from './paltrows-power-toes/App';
-import AppLang from './lang-context/AppLang';
+// import AppLang from './lang-context/AppLang';
+import RegistrationForm from './registrationForm/registrationForm';
 import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <AppLang />
-    </BrowserRouter>, 
+    // <BrowserRouter>
+    //     <AppLang />
+    // </BrowserRouter>, 
+    <RegistrationForm />,
     document.getElementById('root'));
--- src/registrationForm/registrationForm.js
+++ src/registrationForm/registrationForm.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 import './registrationForm.css';
 
-export default class registrationForm extends Component {
+export default class RegistrationForm extends Component {
+    constructor(props){
+        super(props);
+        this.nameInput = React.createRef();
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        // event object way
+        // const name = event.target.name.value;
+        // const password = event.target.password.value;
+        // console.log(name);
+        // console.log(password);
+
+        // React ref way
+        const name = this.nameInput.current.value;
+        console.log(name);
+    }
+
     render(){
         return(
-            <form className='registration'>
-                
+            <form className='registration' onSubmit={e => this.handleSubmit(e)}>
+                <h2>Register</h2>
+                <div className='registration__hint'>* required field</div>
+                <div className='form-group'>
+                    <label htmlFor='name'>Name *</label>
+                    <input type='text' className='registration__control'
+                        name='name' id='name' ref={this.nameInput} defaultValue='Bocephus' />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='password'>Password *</label>
+                    <input type='password' className='registration__control'
+                        name='password' id='password' />
+                        <div className='registration__hint'>
+                            6 to 72 characters
+                            must include a number
+                        </div>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='repeatPassword'>Repeat Password *</label>
+                    <input type='repeatPassword' className='registration__control'
+                        name='repeatPassword' id='repeatPassword' />
+                </div>
+                <div className='registration__button__group'>
+                    <button type='reset' className='registration__button'>Cancel</button>
+                    <button type='submit' className='registration__button'>Save</button>
+                </div>
             </form>
         )
     }
