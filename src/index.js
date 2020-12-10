7071313a69b4920bd421d04a9f3b2835258a59a6
drexelramos <drexelramos@gmail.com>
2020-12-10 05:18:27 -0800

    adding error boundaries

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 // import App from './App';
 // import App from './paltrows-power-toes/App';
 // import AppLang from './lang-context/AppLang';
-import RegistrationForm from './registrationForm/registrationForm';
+// import RegistrationForm from './registrationForm/registrationForm';
+import App from './playingwithdanger/App';
 import './index.css';
 
 ReactDOM.render(
-    // <BrowserRouter>
-    //     <AppLang />
-    // </BrowserRouter>, 
-    <RegistrationForm />,
+    <App />,
     document.getElementById('root'));
